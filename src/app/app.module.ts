e9ac0f1211947987afae8cb1654069cec4755593
algomezmu <algomezmu@gmail.com>
2017-07-11 18:14:03 +0200

    Multiple Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 
 import { FormsModule }   from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeroDetailComponent],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+
+}
